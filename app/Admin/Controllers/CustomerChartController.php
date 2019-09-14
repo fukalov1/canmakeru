dd2ff7cd582ae882511a51e43b1449ce4822abe1
fai <fai_@mail.ru>
2019-09-14 10:03:42 +0400

    добавлена динамика поверки счетчиков клиента

---
+++ app/Admin/Controllers/CustomerChartController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Customer;
+
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+
+class CustomerChartController extends Controller
+{
+
+    public $title = 'Динамика поверок счетчиков ';
+    public $customer_name='';
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        $this->getHeader();
+        return Admin::content(function (Content $content) {
+
+            $id = session('customer_id');
+            $quest  = Customer::join('protokols','customers.id','protokols.customer_id')
+                ->select(\DB::raw('date_format(protokols.protokol_dt, "%Y-%m") as date, count(protokols.protokol_num) count'))
+                ->whereRaw('date_format(protokol_dt, "%Y-%m") <> "0000-00"')
+                ->where('customer_id', $id)
+                ->groupBy(\DB::raw('date_format(protokol_dt, "%Y-%m")'))
+                ->get()->toArray();
+
+            $content->header($this->customer_name);
+            $content->description($this->title);
+
+            $data = ['dinamic' => $quest];
+
+            $content->body(view('admin.charts.customer', $data));
+        });
+    }
+
+    public function getHeader()
+    {
+        $customers = Customer::find(session('customer_id'));
+//        dd($customers->name);
+        $this->customer_name = $customers->name;
+        $this->title .= ' - '.$customers->name;
+//            dd($this->title);
+    }
+
+
+}
--- app/Admin/Controllers/CustomerController.php
+++ app/Admin/Controllers/CustomerController.php
@@ -44,8 +44,75 @@ class CustomerController extends AdminController
         $grid->column('code', __('Код'));
 //        $grid->column('name', __('ФИО'));
         $grid->column('ФИО')->display(function () {
-            return '<a href="/admin/protokols?set='.$this->id.'" title="Протоколы клиента '.$this->name.'">'.$this->name.'</a>';
+            return '<a href="/admin/protokols?set='.$this->id.'" title="Поверки клиента '.$this->name.'">'.$this->name.'</a>';
         });
+        $grid->dinamic('Динамика поверок')->display(function () {
+            return '<a href="/admin/customer_chart?set='.$this->id.'" title="Динамика поверок "><span class="fa fa-bar-chart"/></a>';
+        });
+//        $grid->dinamic('Динамика поверок')->modal('Динамика поверок счетчиков', function ($model) {
+//            $str='';
+//            $id = $model->id;
+//            $quest  = Customer::join('protokols','customers.id','protokols.customer_id')
+//                ->select(\DB::raw('date_format(protokols.protokol_dt, "%Y-%m") as date, count(protokols.protokol_num) count'))
+//                ->whereRaw('date_format(protokol_dt, "%Y-%m") <> "0000-00"')
+//                ->where('customer_id', $id)
+//                ->groupBy(\DB::raw('date_format(protokol_dt, "%Y-%m")'))
+//                ->get()->toArray();
+
+//            $str = "    <div class=\"row\">
+//        <div class=\"col-lg-12\">
+//            <canvas id=\"myChart1\" width=\"800\" height=\"400\"></canvas>
+//            <script>
+//                $(function () {
+//                    var ctx = document.getElementById(\"myChart1\").getContext('2d');
+//                    var myChart1 = new Chart(ctx, {
+//                        type: 'bar',
+//                        data: {
+//                            labels: [";
+//
+//                                foreach($quest as $item) {
+//                                    $str .= "'" . $item['date'] . "',";
+//                                }
+//                            $str .= "],
+//                            datasets: [{
+//                                label: 'Динамика поверки счетчиков',
+//                                data: [";
+//
+//                                    foreach($quest as $item) {
+//                                        $str .= $item['count']. ",";
+//                                    }
+//                                $str .= "],
+//                                backgroundColor: [";
+//                                    foreach($quest as $item) {
+//                                        $str .= "'rgba(54, 162, 235, 0.2)',";
+//                                    }
+//                                $str .= "],
+//                                borderColor: [";
+//                                    foreach($quest as $item) {
+//                                        $str .= "'rgba(255,99,132,1)',";
+//                                    }
+//                                $str .= "],
+//                                borderWidth: 1
+//                            }]
+//                        },
+//                        options: {
+//                            scales: {
+//                                yAxes: [{
+//                                    ticks: {
+//                                        beginAtZero:true
+//                                    }
+//                                }]
+//                            }
+//                        }
+//                    });
+//                });
+//            </script>
+//        </div>
+//    </div>";
+//
+
+//            return $str;
+//        });
         $grid->column('enabled', __('Активен'));
 
         return $grid;
