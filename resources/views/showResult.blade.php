24d9b9d9600ffb65fe569dba81a3fcfa4c45e712
fai <fai_@mail.ru>
2019-09-17 20:16:20 +0400

    fix result show

--- resources/views/showResult.blade.php
+++ resources/views/showResult.blade.php
@@ -25,19 +25,19 @@
                     Фотоматериалы поверки № {{ $number }}
                 </div>
             </div>
-            <div class="col-lg-4 col-sm-4 col-md-4 col-4 text-center">
+            <div class="col-lg-4 col-sm-4 col-md-4 col-12 text-center">
                 <label>Свидетельство</label><br/>
                 <a href="/photo/{{ $protokol_photo }}" target="_blank" title="открыть в оригинальном размере">
                     <img class="myimg" src="/photo/{{ $protokol_photo }}">
                 </a>
             </div>
-            <div class="col-lg-4 col-sm-4 col-md-4 col-4 text-center">
+            <div class="col-lg-4 col-sm-4 col-md-4 col-12 text-center">
                 <label>Свидетельство (обратная сторона)</label><br/>
                 <a href="/photo/{{ $protokol_photo1 }}" target="_blank" title="открыть в оригинальном размере">
                     <img class="myimg" src="/photo/{{ $protokol_photo1 }}">
                 </a>
             </div>
-            <div class="col-lg-4 col-sm-4 col-md-4 col-4 text-center">
+            <div class="col-lg-4 col-sm-4 col-md-4 col-12 text-center">
                 <label>Счетчик</label><br/>
                 <a href="/photo/{{ $meter_photo }}" target="_blank" title="открыть в оригинальном размере">
                     <img class="myimg" src="/photo/{{ $meter_photo }}">
