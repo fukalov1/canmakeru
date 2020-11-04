b5da3f1203fd635c0b1af1dfb472c162b76ba4b4
Fukalov <andrey.fukalov@payoola.com>
2020-11-04 08:08:15 +0400

    fix header auth link

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -16,30 +16,9 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                      <li class="nav-item">
-                          <a class="nav-link" href="/customer">{{ __('Вход') }}</a>
-                      </li>
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Выход') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
+                <li class="nav-item">
+                    <a class="nav-link" href="/customer">{{ __('Личный кабинет') }}  </a>
+                </li>
             </ul>
         </div>
     </div>
