1b5bba6de0f82a2b2de1c978ea5700bfb3416080
Fukalov <andrey.fukalov@payoola.com>
2020-12-18 18:27:23 +0400

    fix mifrate acts

--- database/migrations/2019_11_05_055306_create_table_acts.php
+++ database/migrations/2019_11_05_055306_create_table_acts.php
@@ -15,7 +15,7 @@ class CreateTableActs extends Migration
     {
         Schema::create('acts', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('customer_id')->unsigned();
+            $table->unsignedBigInteger('customer_id');
             $table->string('name')->nullable();
             $table->string('number_act')->unique();
             $table->double('lat')->default(0);
@@ -23,7 +23,7 @@ class CreateTableActs extends Migration
             $table->string('address')->nullable();
             $table->date('date');
             $table->enum('type', ['пригодны','непригодны','испорчен']);
-//            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
             $table->timestamps();
         });
     }
