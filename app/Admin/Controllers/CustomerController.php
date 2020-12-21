f2d285e5cac71141552acbc257f5debe26073051
Fukalov <andrey.fukalov@payoola.com>
2020-12-21 09:29:38 +0400

    fix add  customer with default pass

--- app/Admin/Controllers/CustomerController.php
+++ app/Admin/Controllers/CustomerController.php
@@ -184,6 +184,7 @@ class CustomerController extends AdminController
             $form->text('ideal', __('Эталон'));
             $form->text('ci_as_ideal', __('СИ, как эталон'));
             $form->text('notes', __('Примечание'));
+            $form->hidden('password')->default('123456');
 
         })->tab('Работники', function ($form) {
             $form->hasMany('slave_customers', 'Работники', function (Form\NestedForm $form) {
@@ -199,6 +200,7 @@ class CustomerController extends AdminController
                 $form->text('manufactureNum', 'Заводской номер СИ')->rules('required|max:128');
             });
         });
+
 
 
         return $form;
